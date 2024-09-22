af9a7748528ecb421cd45053544f2dd5300e05cc
33072204+brankobjelic@users.noreply.github.com <33072204+brankobjelic@users.noreply.github.com>
2024-09-23 01:35:12 +0200

    Load data and associate with DataGrid element

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,17 +14,26 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ServisVaga.DAO;
 
 namespace ServisVaga
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        public List<Merilo> Merila { get; set; } = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            DAOConnection.CreateDatabaseFile();
+            Merila = MeriloDAO.UcitajMerila();
+            MerilaDataGrid.ItemsSource = Merila;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
