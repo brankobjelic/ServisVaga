c7c41a1d73add351916d2a43938ed3fe5a7ef831
33072204+brankobjelic@users.noreply.github.com <33072204+brankobjelic@users.noreply.github.com>
2024-09-26 02:14:23 +0200

    Handle click event

--- InsertUpdateMeriloWindow.xaml.cs
+++ InsertUpdateMeriloWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,12 +22,39 @@ namespace ServisVaga
     public partial class InsertUpdateMeriloWindow : Window
     {
         public List<Klijent> Klijenti { get; set; } = new();
+        public List<int> Years { get; set; } = new();
+        int currentYear = DateTime.Now.Year;
 
         public InsertUpdateMeriloWindow()
         {
             InitializeComponent();
+            for(int i = currentYear; i >= 1900; i--)
+            {
+                Years.Add(i);
+            }
+            godinaProizvodnjeComboBox.ItemsSource = Years;
             Klijenti = KlijentDAO.UcitajKlijente();
             klijentiComboBox.ItemsSource = Klijenti;
         }
+
+        private void EnterMeriloData_Click(object sender, RoutedEventArgs e)
+        {
+            Merilo newMerilo = new Merilo()
+            {
+                Imalac = (Klijent)klijentiComboBox.SelectedValue,
+                Naziv = nazivMerilaTextBox.Text,
+                Proizvodjac = proizvodjacTextBox.Text,
+                Tip = tipTextBox.Text,
+                SerijskiBroj = serijskiBrojTextBox.Text,
+                GodinaProizvodnje = (int)godinaProizvodnjeComboBox.SelectedValue,
+                SluzbenaOznaka = sluzbenaOznakaTextBox.Text,
+                OpsegMerenja = opsegMerenjaTextBox.Text,
+                NajmanjiPodeok = najmanjiPodeokTextBox.Text,
+                KlasaTacnosti = klasaTacnostiTextBox.Text
+            };
+            MeriloDAO.UnesiMerilo(newMerilo);
+
+        }
+
     }
 }
