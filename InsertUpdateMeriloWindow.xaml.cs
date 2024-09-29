398b7e80279145b45c5785c19526add80b0e5b90
33072204+brankobjelic@users.noreply.github.com <33072204+brankobjelic@users.noreply.github.com>
2024-09-29 02:01:47 +0200

    Add overloaded contructor

--- InsertUpdateMeriloWindow.xaml.cs
+++ InsertUpdateMeriloWindow.xaml.cs
@@ -22,8 +22,9 @@ namespace ServisVaga
     public partial class InsertUpdateMeriloWindow : Window
     {
         public List<Klijent> Klijenti { get; set; } = new();
-        public List<int> Years { get; set; } = new();
+        public List<long> Years { get; set; } = new();
         int currentYear = DateTime.Now.Year;
+        Merilo MeriloForEdit { get; set; }
 
         public InsertUpdateMeriloWindow()
         {
@@ -35,6 +36,20 @@ namespace ServisVaga
             godinaProizvodnjeComboBox.ItemsSource = Years;
             Klijenti = KlijentDAO.UcitajKlijente();
             klijentiComboBox.ItemsSource = Klijenti;
+        }
+
+        public InsertUpdateMeriloWindow(Merilo meriloForEdit)
+        {
+            InitializeComponent();
+            MeriloForEdit = meriloForEdit;
+            for (long i = currentYear; i >= 1900; i--)
+            {
+                Years.Add(i);
+            }
+            godinaProizvodnjeComboBox.ItemsSource = Years;
+            Klijenti = KlijentDAO.UcitajKlijente();
+            klijentiComboBox.ItemsSource = Klijenti;
+            FillMeriloFields();
         }
 
         private void EnterMeriloData_Click(object sender, RoutedEventArgs e)
@@ -62,6 +77,22 @@ namespace ServisVaga
                 MessageBox.Show("Došlo je do greške");
             }
 
+        }
+
+        private void FillMeriloFields()
+        {
+            List<Klijent> klijentiIzComboBoxa = klijentiComboBox.ItemsSource.Cast<Klijent>().ToList();
+            Klijent izabraniKlijent = klijentiIzComboBoxa.Find(k => k.Id == MeriloForEdit.Imalac.Id);
+            klijentiComboBox.SelectedItem = izabraniKlijent;
+            godinaProizvodnjeComboBox.SelectedValue = MeriloForEdit.GodinaProizvodnje;
+            nazivMerilaTextBox.Text = MeriloForEdit.Naziv;
+            proizvodjacTextBox.Text = MeriloForEdit.Proizvodjac;
+            tipTextBox.Text = MeriloForEdit.Tip;
+            serijskiBrojTextBox.Text = MeriloForEdit.SerijskiBroj;
+            sluzbenaOznakaTextBox.Text = MeriloForEdit.SluzbenaOznaka;
+            opsegMerenjaTextBox.Text = MeriloForEdit.OpsegMerenja;
+            najmanjiPodeokTextBox.Text = MeriloForEdit.NajmanjiPodeok;
+            klasaTacnostiTextBox.Text = MeriloForEdit.KlasaTacnosti;
         }
 
     }
